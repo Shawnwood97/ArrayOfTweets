d1212c33a5727e62897f5bf77c3fe9bb7d74a832
Shawn Wood <Shawnwood97@gmail.com>
2021-03-18 23:14:00 -0600

    pushing 10 tweets update because it took ages to find good ones :)

---
+++ app.js
@@ -0,0 +1,12 @@
+var tweets = [
+  `Getting older. I used to be able to run a 4-minute mile, bench press 380 pounds, and tell the truth.`,
+  `Women get to smell like real things (vanilla, lavender) but men have to smell like concepts. What the fuck is "cool sport rush"`,
+  `Me: i have a headache WebMD: and it’ll be your last`,
+  `Feel bad for the person who has to type all those words so fast every time I turn on subtitles`,
+  `[prounounces molecules like hercules]`,
+  `To the guy who stole my antidepressants, I hope you're happy now.`,
+  `Why don't crabs give to charity? Because they're shellfish. #dadjokes`,
+  `I was addicted to the hokey pokey... But, I turned myself around.`,
+  `Do they allow loud laughing in Hawaii, or just a low ha?`,
+  `Millenials. Walking around like they rent the place.`,
+];
